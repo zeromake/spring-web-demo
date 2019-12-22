053afe3ffee5d81c3e1c80d5df3327be560ec536
zeromake <a390720046@gmail.com>
2019-12-22 14:55:32 +0800

    fix: home mv to controller

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,5 +1,6 @@
 package controllers
 
 import (
+	_ "github.com/zeromake/spring-web-demo/controllers/home"
 	_ "github.com/zeromake/spring-web-demo/controllers/upload"
 )
--- main.go
+++ main.go
@@ -1,30 +1,27 @@
 package main
 
 import (
-	SpringWeb "github.com/go-spring/go-spring-web/spring-web"
-	SpringBoot "github.com/go-spring/go-spring/spring-boot"
-	"net/http"
-
 	_ "github.com/go-spring/go-spring-boot-starter/starter-gin"
 	_ "github.com/go-spring/go-spring-boot-starter/starter-web"
+	SpringBoot "github.com/go-spring/go-spring/spring-boot"
 	_ "github.com/zeromake/spring-web-demo/controllers"
 	_ "github.com/zeromake/spring-web-demo/modules"
 	_ "github.com/zeromake/spring-web-demo/services"
 )
 
-func init() {
-	SpringBoot.RegisterBean(new(Controller))
-}
-
-type Controller struct{}
-
-func (c *Controller) InitWebBean(wc SpringWeb.WebContainer) {
-	wc.GET("/", c.Home)
-}
-
-func (c *Controller) Home(ctx SpringWeb.WebContext) {
-	ctx.String(http.StatusOK, "OK!")
-}
+//func init() {
+//	SpringBoot.RegisterBean(new(Controller))
+//}
+//
+//type Controller struct{}
+//
+//func (c *Controller) InitWebBean(wc SpringWeb.WebContainer) {
+//	wc.GET("/", c.Home)
+//}
+//
+//func (c *Controller) Home(ctx SpringWeb.WebContext) {
+//	ctx.String(http.StatusOK, "OK!")
+//}
 
 func main() {
 	SpringBoot.RunApplication("config/")
